0811bd2815deba2b6c4cd3ae74e3378f4c80dbd4
Ibrahim Shittu <ibshittu01@gmail.com>
2022-02-09 13:38:02 +0100

    update user

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,28 @@
 const router = require('express').Router()
+const {verifyTokenAndAuthorization} = require('../routes/verifyJWT')
+const userModel = require('../models/user')
+const bcrypt = require('bcrypt')
+
 
 router.route('/:id')
-    .put((req, res ) => {
-        console.log('get request')
+    .put(verifyTokenAndAuthorization(), async (req, res ) => {
+
+        if (req.body.password) {
+            req.body.password = await bcrypt.hash(req.body.password, 10)
+        }
+        try {
+            const updatedUser = await userModel.findByIdAndUpdate(req.params.id, {
+                username: req.body.username,
+                email: req.body.email,
+                password: req.body.password
+
+                // $set: req.body
+                
+            }, {new: true})
+            res.send(200).json(updatedUser)
+        } catch (error) {
+            res.sendStatus(403)
+        }
     })
     
 module.exports = router 
--- routes/verifyJWT.js
+++ routes/verifyJWT.js
@@ -3,6 +3,8 @@ const jwt = require('jsonwebtoken');
 const verifyJWT = (req, res, next) => {
     const authHeader = req.headers.authorization;
     if (!authHeader?.startsWith('Bearer ')) return res.sendStatus(401)
+
+    const token = authHeader.split(' ')[1]
    
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
         if (err) {
@@ -17,4 +19,17 @@ const verifyJWT = (req, res, next) => {
     
 }
 
-module.exports = verifyJWT;
+const verifyTokenAndAuthorization = (req, res, next) => {
+    verifyJWT(req, res, () => {
+        if (req.user.id === req.params.id || req.user.isAdmin) {
+            next()
+        } else {
+            res.status(403).json({
+                message: 'Forbidden'
+            });
+        }
+    });
+
+}
+
+module.exports = {verifyJWT, verifyTokenAndAuthorization};
